cf8d8edbe1a9134838879394ca3ab299cac0d972
venkateshreddy <venkateshreddy.muli@gmail.com>
2025-02-08 17:20:43 +0530

    meaningfull msg

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,7 +2,7 @@
 
 echo "please enter your username"
 read -s USERNAME #this is the variable where the value of the one user entered
-echo "enetered username is: $USERNAME
+echo "enetered username is: $USERNAME"
 
 echo "please enter password"
 read -s PASSWORD
