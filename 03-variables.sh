3fab252fcd71b2eb312f77057583c42bb0d6c3df
venkateshreddy <venkateshreddy.muli@gmail.com>
2025-02-08 13:30:53 +0530

    meaningfull msg

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "I am learning devops with AWS"
+PERSION1=$1 #first value at command line
+PERSION2=$2 #second value at command line
+echo "$PERSION1: Hi $PERSION2, good morning"
+echo "$PERSION2: Hi $PERSION1, good morinig"
+echo "$PERSION1: How are you $PERSION2"
+echo "$PERSION2: I'm good, how are you $PERSION1"
