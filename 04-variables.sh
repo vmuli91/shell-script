0fd12918231f791fdc411f25ba045e0774b8b633
venkateshreddy <venkateshreddy.muli@gmail.com>
2025-02-08 16:37:42 +0530

    meaningfull msg

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#we need to develop a programme that can addition of 2 numbers
+
+NUMBER1=$1
+NUMBER2=$2
+#I need to run a command inside a shell to add these two numers
+SUM=((NUMBER1+NUMBER2))
+
+echo "addition of 2numbers is: $SUM"
