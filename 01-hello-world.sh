689b0121cd25a16cf5c0246850c8ab2799602acb
venkateshreddy <venkateshreddy.muli@gmail.com>
2025-02-08 13:04:11 +0530

    meaningfull msg

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "I am learning devops with AWS"
-
-echo "Ramesh: Hi Suresh, good morning"
-echo "Suresh: Hi Ramesh, good morinig"
-echo "Ramesh: How are you Suresh"
-echo "Suresh: I'm good, how are you ramesh"
+PERSION1=RAMESH
+PERSION2=SURESH
+echo "$PERSION1: Hi $PERSION2, good morning"
+echo "$PERSION2: Hi $PERSION1, good morinig"
+echo "$PERSION1: How are you $PERSION2"
+echo "$PERSION2: I'm good, how are you $PERSION1"
