c940047da8010e01f33c8d4856540228e5776852
venkateshreddy <venkateshreddy.muli@gmail.com>
2025-02-08 12:49:49 +0530

    meaningfull msg

---
+++ 01-hello-world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "I am learning devops with AWS"
+
+echo "Ramesh: Hi Suresh, good morning"
+echo "Suresh: Hi Ramesh, good morinig"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I'm good, how are you ramesh"
