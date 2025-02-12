ddf0cd5d6b01853e4524b9a9c233c7b22ef38c7d
venkateshreddy <venkateshreddy.muli@gmail.com>
2025-02-12 10:35:39 +0530

    meaningfull msg

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -4,11 +4,8 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 10 ]
-
-echo "$NUMBER is greater than 10"
-
+then
+    echo "$NUMBER is greater than 10"
 else
-
-echo "$NUMBER is not greater than 10"
-
+    echo "$NUMBER is not greater than 10"
 fi
